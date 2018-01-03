7dc40101ca878cb57d4de3fe65b4786d1296ec21
SeverHo <330967496@qq.com>
2018-01-03 16:45:06 +0800

    临时提交

---
+++ src/lib/shui-wepy.js
@@ -0,0 +1,16 @@
+/*
+* 水sh-ui - wepy
+*/
+
+import wepy from 'wepy';
+import List      from '../components/sh-list';
+import List_item from '../components/sh-list-item';
+
+
+var Page = function(){
+
+}
+
+exports.default = {
+  page:Page
+}
